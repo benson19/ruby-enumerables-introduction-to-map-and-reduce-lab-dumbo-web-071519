8197bb7937d84d3a97add230d9a5f76e04801836
Matthew Guzman <tomaffew@me.com>
2019-07-05 17:46:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,22 @@
 # My Code here....
 
+#negatives
 def map_to_negativize(source_array)
   source_array.map { |n| n * -1 }
 end
 
+#noChange
 def map_to_no_change(source_array)
   source_array
+end
+
+#double
+def map_to_double(source_array)
+  source_array.map { |n| n * 2 }
+end
+
+
+#2
+def map_to_square(source_array)
+  source_array.map { |n| n ** 2 }
 end
