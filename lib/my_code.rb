84ba74c8955cee0692b59d4ee5462d0be1db32df
Matthew Guzman <tomaffew@me.com>
2019-07-05 17:30:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array =[]
+  source_array.length.times do |index|
+    new_array.push(source_array[index] * -1)
+end
+  return new_array
+
